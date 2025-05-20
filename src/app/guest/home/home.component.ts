7a21a8b07657092fe595009c57a834bc7b895dbb
Raúl Morales <rmoralesinformatica@gmail.com>
2025-05-20 21:28:47 +0200

    seccion 3 del home

--- src/app/guest/home/home.component.ts
+++ src/app/guest/home/home.component.ts
@@ -2,10 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: false,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'], // ❗ Era "styleUrl", debe ser styleUrls
 })
 export class HomeComponent {
-
+  serviceCards = [
+    {
+      title: 'Alimentación',
+      description:
+        'Describenos tus gustos y preferencias y te ayudaremos a crear tu alimentación ideal.',
+      image: 'assets/images/manzana.webp',
+    },
+    {
+      title: 'Entrenamiento',
+      description:
+        'Dependiendo de tus objetivos y nivel adaptaremos tu plan de entrenamiento personalizado.',
+      image: 'assets/images/pesa.webp',
+    },
+    {
+      title: 'Progresos',
+      description:
+        'Te mantendrás informado de tus progresos desde que comenzaste, motivándote a seguir adelante.',
+      image: 'assets/images/vascula.webp',
+    }
+  ];
 }
