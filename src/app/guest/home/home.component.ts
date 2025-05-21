c55523e3aa21b8df987f884da7e2cf14aa6adcdf
Raúl Morales <rmoralesinformatica@gmail.com>
2025-05-21 07:00:37 +0200

    secciones 5 y 6

--- src/app/guest/home/home.component.ts
+++ src/app/guest/home/home.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone:false,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
   serviceCards = [
     {
       title: 'Alimentación',
@@ -28,12 +28,49 @@ export class HomeComponent {
     }
   ];
 
+
   galleryIMages = [
     { name: 'progresos', plan: 'Progreso' },
     { name: 'alimentacion', plan: 'Alimentación' },
     { name: 'entrenamiento', plan: 'Entrenamiento' },
     { name: 'food-training', plan: 'Dieta-Entrenamiento' }
   ];
+
+
+  gymFeatures = [
+    'Recibe rutinas de ejercicio diseñadas específicamente para tus objetivos, nivel de condición física y necesidades.',
+    'Un entrenador personal te enseña la forma adecuada de cada ejercicio, reduciendo el riesgo de lesiones.',
+    'Mantente motivado y constante gracias al apoyo y los planes estructurados que te brinda un entrenador personal.'
+  ];
+
+  plansCards = [
+    {
+      id: 'plan_basic',
+      title: 'Plan Básico',
+      description: 'Accede a rutinas semanales y seguimiento básico.',
+      amount: 19.99,
+      image: 'plan_1'
+    },
+    {
+      id: 'plan_plus',
+      title: 'Plan Plus',
+      description: 'Incluye dieta personalizada y feedback mensual.',
+      amount: 39.99,
+      image: 'plan_2'
+    },
+    {
+      id: 'plan_premium',
+      title: 'Plan Premium',
+      description: 'Entrenamiento y nutrición con soporte total.',
+      amount: 59.99,
+      image: 'plan_3'
+    }
+  ];
+
+  subscribe(priceId: string): void {
+    console.log('Usuario quiere suscribirse al plan:', priceId);
+  }
+
 
   trackByIndex(index: number, item: any): number {
     return index;
