4a5110ba70f88dc4748bdea67a318f1e26da4661
Raúl Morales <rmoralesinformatica@gmail.com>
2025-05-21 12:11:43 +0200

    base completa de register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -10,15 +11,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent {
   registerForm: FormGroup;
   submitted = false;
+  successMessage = '';
+  errorMessage = '';
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
-      name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      keyword: ['', [Validators.required, Validators.minLength(4)]]
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      keyword: ['', [Validators.required, Validators.minLength(4)]],
+      birth_date: ['', Validators.required],
+      meals_per_day: ['', [Validators.required, Validators.min(4), Validators.max(7)]],
+      allergies: [''],
+      injuries: [''],
+      gym_goal: ['', Validators.required],
+      training_days: ['', [Validators.required, Validators.min(1), Validators.max(7)]],
+      gender: ['', Validators.required],
+      weight: ['', Validators.required],
+      height: ['', Validators.required],
+      neck: [''],
+      shoulders: [''],
+      chest: [''],
+      waist: [''],
+      hips: [''],
+      thigh: [''],
+      calf: ['']
     });
   }
 
+  // Acceso rápido a los campos del formulario en el HTML
   get f() {
     return this.registerForm.controls;
   }
@@ -27,8 +48,13 @@ export class RegisterComponent {
     this.submitted = true;
 
     if (this.registerForm.invalid) {
+      this.errorMessage = 'Formulario inválido. Revisa los campos obligatorios.';
+      this.successMessage = '';
       return;
     }
 
+    this.successMessage = 'Registro enviado con éxito.';
+    this.errorMessage = '';
+    console.log('Datos enviados:', this.registerForm.value);
   }
 }
