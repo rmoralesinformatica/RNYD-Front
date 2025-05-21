95d17b8475697dfa0854d2ca44b0ce5753dc77b0
Raúl Morales <rmoralesinformatica@gmail.com>
2025-05-21 10:48:14 +0200

    seccion auth/login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
   standalone: false,
+  selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Login:', this.loginForm.value);
+      // Aquí irá la llamada al AuthService cuando lo tengas
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
